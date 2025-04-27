76aea4571e96ee6447f805a05cde1af705c3eb10
Nabil Bouzine <nabilsocialnetwork@gmail.com>
2025-04-27 18:24:58 +0100

    add github workflow to check the test coverage is at least 80%

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,9 @@
+import kotlinx.kover.gradle.plugin.dsl.CoverageUnit
+import kotlinx.kover.gradle.plugin.dsl.GroupingEntityType
+
 plugins {
     kotlin("jvm") version "2.1.10"
+    id("org.jetbrains.kotlinx.kover") version "0.9.1"
 }
 
 group = "net.thechance"
@@ -18,4 +22,18 @@ tasks.test {
 }
 kotlin {
     jvmToolchain(23)
+}
+kover.reports.filters.includes {
+    packages(
+        "net.thechance.data",
+        "net.thechance.logic",
+        "net.thechance.presentation"
+    )
+}
+kover.reports.verify.rule {
+    groupBy = GroupingEntityType.CLASS
+
+    minBound(80, coverageUnits = CoverageUnit.LINE)
+    minBound(80, coverageUnits = CoverageUnit.BRANCH)
+    minBound(80, coverageUnits = CoverageUnit.INSTRUCTION)
 }
