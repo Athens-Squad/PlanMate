e2cac58755ec269b68d8c36640404bf00fb1daf7
Malak Raef <malak.raef20@gmail.com>
2025-04-28 19:39:54 +0300

    implement HashPasswordUseCase and refactor testing for HashPasswordUseCase

--- src/test/kotlin/logic/use_cases/authentication/HashPasswordUseCaseTest.kt
+++ src/test/kotlin/logic/use_cases/authentication/HashPasswordUseCaseTest.kt
@@ -15,17 +15,17 @@ class HashPasswordUseCaseTest {
 
     @Test
     fun `should not produce the same result as the original password when hashed`(){
-        val plainPassword = "123Password"
+        val plainPassword = PASSWORD
 
         val result = hashPasswordUseCase.execute(plainPassword)
 
-        assertThat(result).isNotEqualTo(result)
+        assertThat(result).isNotEqualTo(plainPassword)
     }
 
     @Test
     fun `should hash the password correctly for any input`(){
-        val plainPassword = "123Password"
-        val hashedPassword = "e99a18c428cb38d5f260853678922e03"
+        val plainPassword = PASSWORD
+        val hashedPassword = "d54b609242c7d758f6daca654bda1d26"
 
         val result = hashPasswordUseCase.execute(plainPassword)
 
@@ -34,7 +34,7 @@ class HashPasswordUseCaseTest {
 
     @Test
     fun `should hash the password correctly for each time the same input is given`(){
-        val plainPassword = "123Password"
+        val plainPassword = PASSWORD
 
         val firstResult = hashPasswordUseCase.execute(plainPassword)
         val secondResult = hashPasswordUseCase.execute(plainPassword)
@@ -44,7 +44,7 @@ class HashPasswordUseCaseTest {
 
     @Test
     fun `should produce different hashed passwords for different inputs `(){
-        val firstPlainPassword = "123Password"
+        val firstPlainPassword = PASSWORD
         val secondPlainPassword = "Password123"
 
         val firstResult = hashPasswordUseCase.execute(firstPlainPassword)
@@ -61,6 +61,10 @@ class HashPasswordUseCaseTest {
         val result = hashPasswordUseCase.execute(emptyPassword)
 
         assertThat(result).isEqualTo(hashedPassword)
+    }
+    
+    companion object{
+        const val PASSWORD = "123Password"
     }
 
 }
