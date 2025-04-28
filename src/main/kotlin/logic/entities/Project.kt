436161278c94845c5bb8fcce9adc2548ac221492
Bilal Azzam <blalazam065@gmail.com>
2025-04-28 21:03:41 +0200

    update repositories interface to support Result

--- src/main/kotlin/logic/entities/Project.kt
+++ src/main/kotlin/logic/entities/Project.kt
@@ -4,6 +4,7 @@ import net.thechance.logic.entities.State
 import java.util.UUID
 
 
+
 data class Project(
     val id: String = UUID.randomUUID().toString(),
     val name: String,
@@ -12,6 +13,7 @@ data class Project(
     val tasks: MutableList<Task>,
     val createdBy: String //userId -> Admin
 )
+
 //users
 //projects + states
 //tasks
--- src/main/kotlin/logic/repositories/TasksRepository.kt
+++ src/main/kotlin/logic/repositories/TasksRepository.kt
@@ -4,7 +4,14 @@ import logic.entities.Task
 
 interface TasksRepository {
     fun createTask(task: Task): Result<Unit>
+
     fun updateTask(task: Task): Result<Unit>
+
     fun deleteTask(taskId: String): Result<Unit>
+
+    fun getTaskById(taskId: String): Result<Task>
+
+    fun getTasksByProjectId(projectId: String): Result<List<Task>>
+
     fun getTasks(): Result<List<Task>>
 }
