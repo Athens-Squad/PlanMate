dcd8064c5297d1334fdd7b495ffc845503ebb771
Mohamed Hussein <mohussein1279@gmail.com>
2025-05-01 12:17:44 +0300

    delete unused parser and reader

--- src/main/kotlin/data/aduit_log_csvfile/AuditLogFileReader.kt
+++
@@ -1,9 +0,0 @@
-package net.thechance.data.aduit_log_csvfile
-
-import java.io.File
-
-class AuditLogFileReader(private val file: File) {
-    fun readAuditLogs(): List<String> {
-        return file.useLines { it.drop(1).toList() }
-    }
-}
