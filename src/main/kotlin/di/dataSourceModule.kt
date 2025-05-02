fe9880088911e930937598667542adcf11f3a583
Bilal Azzam <blalazam065@gmail.com>
2025-05-02 09:02:01 +0200

    refactor app module

---
+++ src/main/kotlin/di/dataSourceModule.kt
@@ -0,0 +1,51 @@
+package net.thechance.di
+
+import data.aduit_log_csvfile.data_source.AuditLogDataSource
+import data.aduit_log_csvfile.data_source.AuditLogFileDataSource
+import data.csv_file_handle.CsvFileHandler
+import data.csv_file_handle.CsvFileParser
+import data.tasks.data_source.TasksDataSource
+import data.tasks.data_source.TasksFileDataSource
+import data.user.data_source.UsersDataSource
+import data.user.data_source.UsersFileDataSource
+import logic.entities.AuditLog
+import logic.entities.Task
+import logic.entities.User
+import net.thechance.data.authentication.UserSession
+import org.koin.core.qualifier.named
+import org.koin.dsl.module
+import java.io.File
+
+val dataSourceModule  = module {
+    single(named("tasksCsvFile")) { File("data_files/tasks.csv") }
+    single(named("tasksFileHandler")) { CsvFileHandler(get(named("tasksCsvFile"))) }
+    single(named("tasksFileParser")) { CsvFileParser(factory = Task.Companion::fromCsv) }
+    single<TasksDataSource> {
+        TasksFileDataSource(
+            tasksFileHandler = get(named("tasksFileHandler")),
+            csvFileParser = get(named("tasksFileParser"))
+        )
+    }
+
+    single(named("usersCsvFile")) { File("data_files/users.csv") }
+    single(named("usersFileHandler")) { CsvFileHandler(get(named("usersCsvFile"))) }
+    single(named("usersFileParser")) { CsvFileParser(factory = User.Companion::fromCsv) }
+    single { UserSession() }
+    single<UsersDataSource> {
+        UsersFileDataSource(
+            userFileHandler = get(named("usersFileHandler")),
+            csvFileParser = get(named("usersFileParser"))
+        )
+    }
+
+    single(named("auditLogCsvFile")) { File("data_files/audit_log.csv") }
+    single(named("AuditLogFileHandler")) { CsvFileHandler(get(named("auditLogCsvFile"))) }
+    single(named("AuditLogFileParser")) { CsvFileParser(factory = AuditLog.Companion::fromCsv) }
+    single<AuditLogDataSource> {
+        AuditLogFileDataSource(
+            auditLogFileHandler = get(named("AuditLogFileHandler")),
+            csvFileParser = get(named("AuditLogFileParser"))
+        )
+    }
+
+}
