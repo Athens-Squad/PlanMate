bac97630b3d1a7df897efe9c1f86ce6bc93c271c
Nabil Bouzine <nabilsocialnetwork@gmail.com>
2025-05-02 06:07:27 +0100

    add project dependencies to the appModule

--- src/main/kotlin/di/appModule.kt
+++ src/main/kotlin/di/appModule.kt
@@ -9,9 +9,12 @@ import data.aduit_log_csvfile.data_source.AuditLogFileDataSource
 import data.authentication.utils.PasswordHashing
 import data.csv_file_handle.CsvFileHandler
 import data.csv_file_handle.CsvFileParser
+import data.projects.ProjectsDataSource
+import data.projects.ProjectsFileDataSource
 import data.tasks.data_source.TasksDataSource
 import data.tasks.data_source.TasksFileDataSource
 import data.user.data_source.UsersDataSource
+import logic.entities.Project
 import logic.use_cases.task.taskvalidations.TaskValidator
 import logic.use_cases.task.taskvalidations.TaskValidatorImpl
 import org.koin.core.qualifier.named
@@ -63,6 +66,21 @@ val appModule = module {
         AuditLogFileDataSource(
             auditLogFileHandler = get(named("AuditLogFileHandler")),
             csvFileParser = get(named("AuditLogFileParser"))
+        )
+    }
+
+    single(named("projectsCsvFile")) { File("data_files/projects.csv") }
+
+    single(named("projectsFileHandler")) { CsvFileHandler(get(named("projectsCsvFile"))) }
+
+    single(named("projectsFileParser")) { CsvFileParser(factory = Project.Companion::fromCsv) }
+
+
+    single<ProjectsDataSource> {
+        ProjectsFileDataSource(
+            projectsFileHandler = get(named("projectsFileHandler")),
+            csvFileParser = get(named("projectsFileParser")),
+            tasksFileDataSource = get()
         )
     }
 
