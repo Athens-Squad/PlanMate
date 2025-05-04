fbb333403044bf6e8572c450f2149d83af1225e4
Bilal Azzam <blalazam065@gmail.com>
2025-05-04 23:36:13 +0200

    setup mongoModule

---
+++ src/main/kotlin/di/mongoModule.kt
@@ -0,0 +1,16 @@
+package net.thechance.di
+
+import com.mongodb.kotlin.client.coroutine.MongoClient
+import com.mongodb.kotlin.client.coroutine.MongoDatabase
+import org.koin.dsl.module
+
+val mongoModule = module {
+    single<MongoClient> {
+        MongoClient.create("mongodb+srv://athens:<db_password>@planmatecluster.caw6br8.mongodb.net/?retryWrites=true&w=majority&appName=PlanMateCluster")
+    }
+
+    single<MongoDatabase> {
+        get<MongoClient>().getDatabase("planmate")
+    }
+
+}
