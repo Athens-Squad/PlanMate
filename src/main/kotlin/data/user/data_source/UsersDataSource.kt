530d4af1718fa6a67650613ced40fb20e4519121
Doaa Abdelnaser <doaaabdelnaser151998@gmail.com>
2025-05-05 21:24:26 +0300

    refactor logic and data for authentication without  Result Return type

--- src/main/kotlin/data/user/data_source/UsersDataSource.kt
+++ src/main/kotlin/data/user/data_source/UsersDataSource.kt
@@ -3,7 +3,7 @@ package data.user.data_source
 import logic.entities.User
 
 interface UsersDataSource {
-    fun createUser(user: User): Result<Unit>
-    fun getUserByUsername(userName: String): Result<User>
+    fun createUser(user: User): Unit
+    fun getUserByUsername(userName: String): User
     fun getAllUsers(): List<User>
 }
