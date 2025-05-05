2a99d8fe054ec8ab748368b6cc305ddc8f7c6bea
Bilal Azzam <blalazam065@gmail.com>
2025-05-06 01:04:03 +0200

    migrate projects to mongo db

--- src/main/kotlin/data/projects/datasource/remote/mongo/MongoProjectDataSource.kt
+++ src/main/kotlin/data/projects/datasource/remote/mongo/MongoProjectDataSource.kt
@@ -1,33 +1,60 @@
 package net.thechance.data.projects.datasource.remote.mongo
 
+import com.mongodb.client.model.Filters
 import com.mongodb.kotlin.client.coroutine.MongoCollection
 import data.states.data_source.StatesDataSource
 import data.tasks.data_source.TasksDataSource
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.toList
+import logic.entities.ProgressionState
 import logic.entities.Project
 import net.thechance.data.projects.datasource.ProjectsDataSource
 import net.thechance.data.projects.dto.ProjectDto
 
 class MongoProjectDataSource(
-    private val collection: MongoCollection<ProjectDto>,
+    private val projectsCollection: MongoCollection<ProjectDto>,
     private val tasksDataSource: TasksDataSource,
     private val statesDataSource: StatesDataSource
-): ProjectsDataSource {
+) : ProjectsDataSource {
     override suspend fun createProject(project: Project) {
         val projectDto = ProjectDto.fromProject(project)
 
-        collection.insertOne(projectDto)
+        projectsCollection.insertOne(projectDto)
     }
 
     override suspend fun updateProject(project: Project) {
-        TODO("Not yet implemented")
+        val updatedProjectDto = ProjectDto.fromProject(project)
+
+        val queryParams = Filters.eq("_id", project.id)
+
+        projectsCollection.replaceOne(queryParams, updatedProjectDto)
     }
 
     override suspend fun deleteProject(projectId: String) {
-        TODO("Not yet implemented")
+        val queryParams = Filters.eq("_id", projectId)
+
+        projectsCollection.deleteOne(queryParams)
     }
 
     override suspend fun getProjects(): List<Project> {
-        TODO("Not yet implemented")
+        return projectsCollection.find()
+            .map { projectDto: ProjectDto ->
+                val project = projectDto.toProject()
+
+                val progressionStates = statesDataSource.getStates()
+                    .filter { state: ProgressionState ->
+                        state.projectId == project.id
+                    }
+                    .toMutableList()
+
+                val tasks = tasksDataSource.getTasksByProjectId(project.id).toMutableList()
+
+                project.copy(
+                    progressionStates = progressionStates,
+                    tasks = tasks
+                )
+            }
+            .toList()
     }
 
 }
