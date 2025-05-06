e3e5522bdd019e0b1912da3bf17daf015689566e
Bilal Azzam <blalazam065@gmail.com>
2025-05-06 19:49:25 +0200

    refactor user type serialization

--- src/main/kotlin/data/user/data_source/remote/UserDto.kt
+++ src/main/kotlin/data/user/data_source/remote/UserDto.kt
@@ -1,11 +1,6 @@
 package net.thechance.data.user.data_source.remote
 
-import data.user.utils.UserColumnIndex.ID
-import data.user.utils.UserColumnIndex.PASSWORD
-import data.user.utils.UserColumnIndex.USERNAME
-import data.user.utils.UserColumnIndex.USER_NAME_STARTING_INDEX
-import data.user.utils.UserColumnIndex.USER_TYPE
-import logic.CsvSerializable
+
 import logic.entities.User
 import logic.entities.UserType
 import org.bson.codecs.pojo.annotations.BsonId
@@ -15,19 +10,37 @@ data class UserDto(
     @BsonId val id: String = UUID.randomUUID().toString(),
     val name: String,
     val password: String,
-    val type: UserType
+    val userType: String,
+    val adminName: String? = null
 ) {
     fun toUser() = User(
         id = id, name = name, password = password,
-        type = type
+        type = when(userType) {
+            "Mate" -> UserType.MateUser(adminName ?: throw Exception("Couldn't find the admin"))
+            "Admin" -> UserType.AdminUser
+            else -> throw Exception("Invalid User Type")
+        }
     )
 
     companion object {
-        fun fromUser(user: User)=UserDto(
-            id=user.id,
-            name = user.name,
-            password = user.password,
-            type = user.type
-        )
+
+        fun fromUser(user: User): UserDto {
+            return when(user.type) {
+                is UserType.AdminUser -> UserDto(
+                    id = user.id,
+                    name = user.name,
+                    password = user.password,
+                    userType = "Admin"
+                )
+
+                is UserType.MateUser -> UserDto(
+                    id = user.id,
+                    name = user.name,
+                    password = user.password,
+                    userType = "Mate",
+                    adminName = user.type.adminName
+                )
+            }
+        }
     }
 }
