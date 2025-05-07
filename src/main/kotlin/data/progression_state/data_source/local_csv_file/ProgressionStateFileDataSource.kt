0c5b49bb2f1d05de0349278790c1a7ab6cfb39fa
Malak Raef <malak.raef20@gmail.com>
2025-05-07 14:28:47 +0300

    refactor ProgressionState entity to extends CsvSerializable

--- src/main/kotlin/data/progression_state/data_source/local_csv_file/ProgressionStateFileDataSource.kt
+++ src/main/kotlin/data/progression_state/data_source/local_csv_file/ProgressionStateFileDataSource.kt
@@ -10,36 +10,36 @@ import net.thechance.data.progression_state.mappers.toProgressionStateDto
 
 class ProgressionStateFileDataSource(
 	private val progressionStatesFileHandler: CsvFileHandler,
-	private val csvFileParser: CsvFileParser<ProgressionStateDto>
+	private val csvFileParser: CsvFileParser<ProgressionState>
 ) : ProgressionStateDataSource {
 
 	override suspend fun createProgressionState(progressionState: ProgressionState) {
-		val record = csvFileParser.toCsvRecord(progressionState.toProgressionStateDto())
+		val record = csvFileParser.toCsvRecord(progressionState)
 		progressionStatesFileHandler.appendRecord(record)
 	}
 
 	override suspend fun updateProgressionState(progressionState: ProgressionState) {
 		val updateState = getProgressionStates()
 			.map { if (it.id == progressionState.id) progressionState else it }
-		val updatedRecords = updateState.map { csvFileParser.toCsvRecord(it.toProgressionStateDto()) }
+		val updatedRecords = updateState.map { csvFileParser.toCsvRecord(it) }
 		progressionStatesFileHandler.writeRecords(updatedRecords)
 	}
 
 	override suspend fun deleteProgressionState(progressionStateId: String) {
 		val updatedState = getProgressionStates()
 			.filter { it.id != progressionStateId }
-		val updatedRecords = updatedState.map { csvFileParser.toCsvRecord(it.toProgressionStateDto()) }
+		val updatedRecords = updatedState.map { csvFileParser.toCsvRecord(it) }
 		progressionStatesFileHandler.writeRecords(updatedRecords)
 	}
 
 	override suspend fun getProgressionStates(): List<ProgressionState> {
 		return progressionStatesFileHandler.readRecords()
-			.map { csvFileParser.parseRecord(it).toProgressionState() }
+			.map { csvFileParser.parseRecord(it) }
 	}
 
 	override suspend fun getProgressionStatesByProjectId(projectId: String): List<ProgressionState> {
 		return progressionStatesFileHandler.readRecords()
-			.map { csvFileParser.parseRecord(it).toProgressionState() }
+			.map { csvFileParser.parseRecord(it) }
 			.filter { it.projectId == projectId }
 	}
 }
