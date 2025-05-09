79884b27d16fbe1717f26d639e5e42aa8e07a275
Mohamed Ragab Ali <mohamed.ragab.ali.9@gmail.com>
2025-05-09 18:01:18 +0300

    inject the projectValidator

--- src/main/kotlin/di/appModule.kt
+++ src/main/kotlin/di/appModule.kt
@@ -8,6 +8,8 @@ import logic.use_cases.task.taskvalidations.TaskValidatorImpl
 import net.thechance.logic.use_cases.authentication.uservalidation.UserValidator
 import net.thechance.logic.use_cases.authentication.uservalidation.UserValidatorImpl
 import net.thechance.logic.use_cases.progression_state.progressionStateValidations.ProgressionStateValidator
+import net.thechance.logic.use_cases.project.projectValidations.ProjectValidator
+import net.thechance.logic.use_cases.project.projectValidations.ProjectValidatorImp
 import org.koin.dsl.module
 
 val appModule = module {
@@ -15,8 +17,8 @@ val appModule = module {
 	single<TaskValidator> { TaskValidatorImpl(get(), get(), get()) }
 	single<ProgressionStateValidator> { ProgressionStateValidatorImpl(get(), get()) }
 	single<UserValidator> { UserValidatorImpl(get()) }
+	single <ProjectValidator>{ ProjectValidatorImp()  }
 
 	single { PasswordHashing() }
-
 
 }
