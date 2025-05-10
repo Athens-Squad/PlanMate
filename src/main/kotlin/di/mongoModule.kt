a50e376958dc895b21650d8abacd455e653ee2a0
Bilal Azzam <blalazam065@gmail.com>
2025-05-11 00:15:35 +0200

    reduce duplication in di

--- src/main/kotlin/di/mongoModule.kt
+++ src/main/kotlin/di/mongoModule.kt
@@ -1,76 +1,67 @@
 package net.thechance.di
 
-import com.mongodb.ConnectionString
-import com.mongodb.MongoClientSettings
 import com.mongodb.kotlin.client.coroutine.MongoClient
+import com.mongodb.kotlin.client.coroutine.MongoCollection
 import com.mongodb.kotlin.client.coroutine.MongoDatabase
 import net.thechance.data.aduit_log.data_source.remote.mongo.dto.AuditLogDto
 import net.thechance.data.progression_state.data_source.remote.mongo.dto.ProgressionStateDto
 import data.projects.data_source.remote.mongo.dto.ProjectDto
 import net.thechance.data.tasks.data_source.remote.mongo.dto.TaskDto
 import net.thechance.data.user.data_source.remote.mongo.dto.UserDto
-import net.thechance.data.utils.UuidCodecProvider
+import net.thechance.data.utils.MongoProvider
 import net.thechance.data.utils.loadEnvironmentVariable
-import org.bson.codecs.configuration.CodecRegistries.fromProviders
-import org.bson.codecs.configuration.CodecRegistries.fromRegistries
-import org.koin.core.module.Module
 import org.koin.core.qualifier.named
 import org.koin.dsl.module
 
 val mongoModule = module {
 
-    fun <T : Any> Module.mongoCollection(
-        collectionName: String,
-        bindingName: String,
-        documentClass: Class<T>
-    ) {
-        single(named(bindingName)) {
-            get<MongoDatabase>().getCollection(collectionName, documentClass)
-        }
-    }
-
     single<MongoClient> {
-        MongoClient.create(
-            MongoClientSettings.builder()
-                .applyConnectionString(ConnectionString(loadEnvironmentVariable("MONGODB_URI")))
-                .codecRegistry(
-                    fromRegistries(
-                        fromProviders(UuidCodecProvider()),
-                        MongoClientSettings.getDefaultCodecRegistry()
-                    )
-                )
-                .build()
-        )
+        MongoProvider.createClient(loadEnvironmentVariable("MONGODB_URI"))
     }
 
     single<MongoDatabase> {
         get<MongoClient>().getDatabase("planmate")
     }
 
-    mongoCollection(
-        collectionName = "projects",
-        bindingName = "projectsCollection",
-        documentClass = ProjectDto::class.java
-    )
-    mongoCollection(
-        collectionName = "progression_states",
-        bindingName = "progressionStatesCollection",
-        documentClass = ProgressionStateDto::class.java
-    )
-    mongoCollection(
-        collectionName = "tasks",
-        bindingName = "tasksCollection",
-        documentClass = TaskDto::class.java
-    )
-    mongoCollection(
-        collectionName = "audit_log",
-        bindingName = "auditLogsCollection",
-        documentClass = AuditLogDto::class.java
-    )
-    mongoCollection(
-        collectionName = "users",
-        bindingName = "usersCollection",
-        documentClass = UserDto::class.java
-    )
+
+    single<MongoCollection<UserDto>>(named("usersCollection")) {
+        MongoProvider.getCollection(
+            database = get(),
+            collectionName = "users",
+            documentClass = UserDto::class.java
+        )
+    }
+
+    single<MongoCollection<ProjectDto>>(named("projectsCollection")) {
+        MongoProvider.getCollection(
+            database = get(),
+            collectionName = "projects",
+            documentClass = ProjectDto::class.java
+        )
+    }
+
+    single<MongoCollection<TaskDto>>(named("tasksCollection")) {
+        MongoProvider.getCollection(
+            database = get(),
+            collectionName = "tasks",
+            documentClass = TaskDto::class.java
+        )
+    }
+
+    single<MongoCollection<ProgressionStateDto>>(named("progressionStatesCollection")) {
+        MongoProvider.getCollection(
+            database = get(),
+            collectionName = "progression_states",
+            documentClass = ProgressionStateDto::class.java
+        )
+    }
+
+    single<MongoCollection<AuditLogDto>>(named("auditLogsCollection")) {
+        MongoProvider.getCollection(
+            database = get(),
+            collectionName = "audit_logs",
+            documentClass = AuditLogDto::class.java
+        )
+    }
 
 }
