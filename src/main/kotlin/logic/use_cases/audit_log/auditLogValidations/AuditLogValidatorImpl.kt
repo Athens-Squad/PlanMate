a6c8970498b84abc0d9159255ae86ac2f15a9420
Doaa Abdelnaser <doaaabdelnaser151998@gmail.com>
2025-05-12 00:02:39 +0300

    refactor exceptions

--- src/main/kotlin/logic/use_cases/audit_log/auditLogValidations/AuditLogValidatorImpl.kt
+++ src/main/kotlin/logic/use_cases/audit_log/auditLogValidations/AuditLogValidatorImpl.kt
@@ -1,4 +1,6 @@
-@file:OptIn(ExperimentalUuidApi::class, ExperimentalUuidApi::class, ExperimentalUuidApi::class)
+@file:OptIn(ExperimentalUuidApi::class, ExperimentalUuidApi::class, ExperimentalUuidApi::class,
+	ExperimentalUuidApi::class
+)
 
 package net.thechance.logic.use_cases.audit_log.auditLogValidations
 
