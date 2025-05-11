d5708ded8d2948638e9cafddb67b3539eb7d9b59
Doaa Abdelnaser <doaaabdelnaser151998@gmail.com>
2025-05-11 23:49:25 +0300

    refactor exceptions

--- src/main/kotlin/logic/use_cases/progression_state/progressionStateValidations/ProgressionStateValidatorImpl.kt
+++ src/main/kotlin/logic/use_cases/progression_state/progressionStateValidations/ProgressionStateValidatorImpl.kt
@@ -14,6 +14,7 @@ import kotlin.uuid.ExperimentalUuidApi
 import kotlin.uuid.Uuid
 
 
+@OptIn(ExperimentalUuidApi::class)
 class ProgressionStateValidatorImpl(
     private val projectsRepository: ProjectsRepository,
     private val progressionStateRepository: ProgressionStateRepository
