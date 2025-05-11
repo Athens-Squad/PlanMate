621394867675a498d6f2a21c845142a22a9d007e
Mohamed Hussein <mohussein1279@gmail.com>
2025-05-11 22:03:07 +0300

    create test for ProgressionStateFileDataSource and refactor mapper

--- src/main/kotlin/data/progression_state/data_source/localCsvFile/mapper/ProgressionStateCsvMapper.kt
+++ src/main/kotlin/data/progression_state/data_source/localCsvFile/mapper/ProgressionStateCsvMapper.kt
@@ -5,21 +5,19 @@ package data.progression_state.data_source.localCsvFile.mapper
 import data.progression_state.data_source.localCsvFile.dto.ProgressionStateCsvDto
 import logic.entities.ProgressionState
 import kotlin.uuid.ExperimentalUuidApi
-import kotlin.uuid.Uuid
 
-@OptIn(ExperimentalUuidApi::class)
 fun ProgressionStateCsvDto.toProgressionState(): ProgressionState {
     return ProgressionState(
-        id = Uuid.random(),
+        id = id,
         name = name,
-        projectId = Uuid.random()
+        projectId = projectId
     )
 }
 
 fun ProgressionState.toProgressionStateCsvDto(): ProgressionStateCsvDto {
     return ProgressionStateCsvDto(
-        id = Uuid.random(),
+        id = id,
         name = name,
-        projectId = Uuid.random()
+        projectId = projectId
     )
 }
