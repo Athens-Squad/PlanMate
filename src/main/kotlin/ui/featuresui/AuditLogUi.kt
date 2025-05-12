591a723095cb7b89200d303724386dcab38d66cc
Bilal Azzam <blalazam065@gmail.com>
2025-05-12 16:26:42 +0200

    refactor AuditLogUi

--- src/main/kotlin/ui/featuresui/AuditLogUi.kt
+++ src/main/kotlin/ui/featuresui/AuditLogUi.kt
@@ -24,41 +24,46 @@ class AuditLogUi(
 
     suspend fun getTaskHistory(taskId: Uuid): List<AuditLog> {
         consoleIO.printer.printText("Here is The History of Your Task", TextStyle.TITLE)
+
         return auditLogUseCases.getAuditLogsByTaskIdUseCase.execute(taskId)
     }
 
     suspend fun getProjectHistory(projectId: Uuid): List<AuditLog> {
         consoleIO.printer.printText("Here is The History of Your Project", TextStyle.TITLE)
+
         return auditLogUseCases.getAuditLogsByProjectIdUseCase.execute(projectId)
     }
 
-    private suspend fun clearLog() {
-        auditLogUseCases.clearLogUseCase.execute()
-    }
+
 
 
     fun showHistoryOption() {
         consoleIO.printer.printText("Select Option (1 , 2 )", TextStyle.TITLE)
         consoleIO.printer.printOptions(AuditLogOptions.entries)
+
         val inputHistoryOption = consoleIO.reader.readNumberFromUser()
-
         when (inputHistoryOption) {
-            AuditLogOptions.CLEAR_LOG.optionNumber -> {
-                logScope.launch {
-                    try {
-                        clearLog()
-                        consoleIO.printer.printText("History Deleted Successfully.", TextStyle.SUCCESS)
-                    } catch (exception: Exception) {
-                        consoleIO.printer.printText("Error : ${exception.message}", TextStyle.ERROR)
-                    }
-                }
-
-            }
+            AuditLogOptions.CLEAR_LOG.optionNumber -> clearHistory()
 
             AuditLogOptions.BACK.optionNumber -> {
                 return
             }
         }
 
+    }
+
+    private fun clearHistory() {
+        logScope.launch {
+            try {
+                clearLog()
+                consoleIO.printer.printText("History Deleted Successfully.", TextStyle.SUCCESS)
+            } catch (exception: Exception) {
+                consoleIO.printer.printText("Error: ${exception.message}", TextStyle.ERROR)
+            }
+        }
+    }
+
+    private suspend fun clearLog() {
+        auditLogUseCases.clearLogUseCase.execute()
     }
 }
