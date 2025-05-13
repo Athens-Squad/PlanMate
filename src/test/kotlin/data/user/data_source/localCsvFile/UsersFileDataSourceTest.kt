901d20f05eea95be7d903f807eed95490fdff5ca
Bilal Azzam <blalazam065@gmail.com>
2025-05-13 12:39:31 +0200

    test the user data

--- src/test/kotlin/data/user/data_source/localCsvFile/UsersFileDataSourceTest.kt
+++ src/test/kotlin/data/user/data_source/localCsvFile/UsersFileDataSourceTest.kt
@@ -2,17 +2,22 @@
 
 package data.user.data_source.localCsvFile
 
+
+import data.user.data_source.remote.mongo.mapper.toUserDto
 import data.utils.csv_file_handle.CsvFileHandler
 import data.utils.csv_file_handle.CsvFileParser
 import helper.authentication_helper.FakeUser
 import io.mockk.*
 import kotlinx.coroutines.test.runTest
+import logic.exceptions.UserAlreadyExistsException
+import logic.exceptions.UserNotFoundException
 import net.thechance.data.user.data_source.localCsvFile.UsersFileDataSource
 import net.thechance.data.user.data_source.localCsvFile.dto.UserCsvDto
+import net.thechance.data.user.data_source.localCsvFile.mapper.toUser
 import net.thechance.data.user.data_source.localCsvFile.mapper.toUserCsvDto
-import kotlin.test.BeforeTest
-import kotlin.test.Test
+import kotlin.test.*
 import kotlin.uuid.ExperimentalUuidApi
+
 
 class UsersFileDataSourceTest {
 
@@ -39,5 +44,40 @@ class UsersFileDataSourceTest {
 
         coVerify { fileHandler.appendRecord(csvRecord) }
     }
+
+    @Test
+    fun `should throw UserAlreadyExistsException when user already exists`() = runTest {
+        coEvery { fileHandler.readRecords() } returns listOf(csvRecord)
+        every { parser.parseRecord(csvRecord) } returns dto
+
+        val exception = assertFailsWith<UserAlreadyExistsException> {
+            dataSource.createUser(user, password)
+        }
+
+        assertTrue(exception.message == "User already exists")
+    }
+
+    @Test
+    fun `should return user when exists`() = runTest {
+        coEvery { fileHandler.readRecords() } returns listOf(csvRecord)
+        every { parser.parseRecord(csvRecord) } returns dto
+
+        val result = dataSource.getUserByUsername(user.name)
+
+        assertEquals(user, result)
+    }
+
+    @Test
+    fun `should throw UserNotFoundException when not found`() = runTest {
+        coEvery { fileHandler.readRecords() } returns listOf(csvRecord)
+        every { parser.parseRecord(csvRecord) } returns dto
+
+        val exception = assertFailsWith<UserNotFoundException> {
+            dataSource.getUserByUsername("Ahmed")
+        }
+
+        assertEquals(exception.message, "User not found")
+    }
+
 
 }
