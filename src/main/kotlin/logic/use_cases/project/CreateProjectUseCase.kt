3ca13034ba7c7cfc338ca743473028f57ed8f64a
Mohamed Ragab Ali <mohamed.ragab.ali.9@gmail.com>
2025-05-13 17:40:58 +0300

    refactor create project use case

--- src/main/kotlin/logic/use_cases/project/CreateProjectUseCase.kt
+++ src/main/kotlin/logic/use_cases/project/CreateProjectUseCase.kt
@@ -12,25 +12,25 @@ import java.time.LocalDateTime
 import kotlin.uuid.ExperimentalUuidApi
 
 class CreateProjectUseCase(
-	private val projectRepository: ProjectsRepository,
-	private val projectValidator: ProjectValidator,
-	private val createAuditLogUseCase: CreateAuditLogUseCase,
+    private val projectRepository: ProjectsRepository,
+    private val projectValidator: ProjectValidator,
+    private val createAuditLogUseCase: CreateAuditLogUseCase,
 ) {
-	suspend fun execute(project: Project) {
-		projectValidator.validateProjectFieldsNotBlank(project)
-		projectValidator.validateUserIsAuthorized(project.createdByUserName)
-		projectValidator.validateProjectNotExists(project.id)
+    suspend fun execute(project: Project) {
+        projectValidator.validateProjectFieldsNotBlank(project)
+        projectValidator.validateUserIsAuthorized(project.createdByUserName)
+        projectValidator.validateProjectNotExists(project.id)
 
-		projectRepository.createProject(project)
+        projectRepository.createProject(project)
 
-		createAuditLogUseCase.execute(
-			AuditLog(
-				entityType = EntityType.PROJECT,
-				entityId = project.id,
-				description = "Project created successfully.",
-				userName = project.createdByUserName,
-				createdAt = LocalDateTime.now(),
-			)
-		)
-	}
+        createAuditLogUseCase.execute(
+            AuditLog(
+                entityType = EntityType.PROJECT,
+                entityId = project.id,
+                description = "Project created successfully.",
+                userName = project.createdByUserName,
+                createdAt = LocalDateTime.now(),
+            )
+        )
+    }
 }
